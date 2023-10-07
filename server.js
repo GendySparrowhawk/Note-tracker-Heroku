2bf8130bece3f2e2cb090c51725319242f82d420
jacob hoefer <jacob.hoefer@gmail.com>
2023-10-07 15:29:02 -0400

    added dynamic port adn heroku settings

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 const dbTools = require('./db')
 const { v4: uuidv4 } = require('uuid');
+const PORT = process.env.PORT || 3333;
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -66,9 +67,9 @@ if (noteIndex === -1) {
 notes.splice(noteIndex, 1);
 dbTools.writeData(notes);
 
-res.json({ message: 'Note removessuccessfully'})
+res.json({message: 'Note removed successfully'})
 })
 
 // Start the server and listen on port 3333
-app.listen(3333, () => console.log('Server started on port 3333'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 

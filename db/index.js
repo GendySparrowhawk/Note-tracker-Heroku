ecc7e45d4a5a1bf151bca3d9b1d83a286c87a706
jacob hoefer <jacob.hoefer@gmail.com>
2023-10-07 15:44:30 -0400

    deploy ready

--- db/index.js
+++ db/index.js
@@ -1,18 +1,20 @@
+// pulling in requirments for paths and such
+
 const fs = require('fs');
 const path = require('path');
 const dbPath = path.join(__dirname, './db.json');
-
+// retrive data from the database
 function getData() {
   const json = fs.readFileSync(dbPath, 'utf8');
 
   return JSON.parse(json);
 }
-
+// write new data from the database
 function writeData(notesArray) {
   fs.writeFile(dbPath, JSON.stringify(notesArray, null, 2), () => {
     console.log('DB updated successfully!');
   });
 }
 
-
+// export these functions to be used on server.js
 module.exports = { getData, writeData }
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 
-
+// pulling in express and other dependecies 
 const express = require('express');
 const path = require('path');
 const app = express();
 const dbTools = require('./db')
 const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 3333;
-
+// middleware path
 app.use(express.static('public'));
 app.use(express.json());
 
-
+// listens for landing pagea nd sends response index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 
 });
-
+// listens for notes.html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 
 });
-
+// listens for the notes array from database and allows the front end to write the data into the fields required.
 app.get('/api/notes/:notes', (req, res) => {
   const noteData = dbTools.getData();
   const note = req.params.note
@@ -37,17 +37,19 @@ app.get('/api/notes/:notes', (req, res) => {
 
 });
 
-
+// listen for the note data aka the note array in the db.json
 app.get('/api/notes', (req, res) => {
   const noteData = dbTools.getData();
   res.json(noteData);
 
 })
-
+// listens for the post funciton which allows the user on the front end to enter new data
 app.post('/api/notes', (req, res) => {
   const notes = dbTools.getData();
   const newNote = req.body;
+  // writes an randomly generated id using uuid so that it can be dleted if need be.
   newNote.id = uuidv4();
+
 
   notes.push(newNote);
   dbTools.writeData(notes);
@@ -55,6 +57,7 @@ app.post('/api/notes', (req, res) => {
   res.json({ message: 'Note added successfully', note: newNote });
 });
 
+// delete function that listens for the id assigned above and deletes it when the user selects that. 
 app.delete('/api/notes/:id', (req, res) => {
   const notes = dbTools.getData();
 
@@ -63,7 +66,7 @@ if (noteIndex === -1) {
   res.json( {message: 'No id found'});
   return 
 }
-
+// cuts the note index at the right number and then writes the data once more. 
 notes.splice(noteIndex, 1);
 dbTools.writeData(notes);
 
